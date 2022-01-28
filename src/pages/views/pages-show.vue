fc4efc4ca604b538958d28966dafc0c9d4b93552
Ryan Harmon <heyharmon@gmail.com>
2022-01-28 15:00:22 -0700

    Add component library. Render hero.

--- src/pages/views/pages-show.vue
+++ src/pages/views/pages-show.vue
@@ -7,10 +7,18 @@
         <div class="margin-bottom-md">
             <p>Slug: {{ store.page.slug }}</p>
         </div>
+        
+        <Hero
+          label="This is label"
+          header="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+          sub-header="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam vero aliquam, natus quae architecto hic quod!"
+          backgroundImageSrc="https://img.wallpapersafari.com/desktop/1024/576/90/72/qQ6bx0.png"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
+import { Hero, ContentComponent, CodyButton } from 'v-codyhouse-componentz'
 import { usePagesStore } from '@/pages/store'
 
 const store = usePagesStore()
